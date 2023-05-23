b03dfcfcac92844d2fa02ccf9835d225cf952ae1
Cecilia Dagata <csdagata@gmail.com>
2023-05-22 21:06:35 -0300

    correcciones en revisión para re entrega

--- js/index.js
+++ js/index.js
@@ -1,20 +1,56 @@
 /*Se realizan los cálculos para una sola de las temáticas*/
+let confirmarInicio = confirm ("Bienvenid@. Para armar tu plan de viaje necesitamos establecer qué gastos quieres realizar.")
+if (confirmarInicio === true){
+    ingresarEmail()
+}else{
+    console.log("Vemos que no te has decidido. Si necesitas ayuda utiliza la sección de contacto y a la brevedad nos comunicaremos contigo.")
+}
+
+function ingresarEmail() {
+    userEmail = prompt("Ingresa un e-mail para poder: \n -enviarte información de presupuesto \n -factura con el detalle de tu compra")
+    if (userEmail.trim().toLowerCase() != "" && userEmail.includes("@")) {
+        alert("Los datos que ingresas quedan resguardados por nuestra política de información.")
+    } else {
+        console.warn("No pudimos validar el email ingresado. Reinténtalo.")
+        ingresarEmail()
+    } return userEmail
+}
+
+function seleccionarTour() {
+    let eleccionTour = parseInt(prompt("A continuación ingresa el código del tour elegido"))
+
+    if (eleccionTour === tourA) {
+        console.log("Elegiste el tour con temática: Intervención humana")
+    } else if (eleccionTour === tourB) {
+        console.log("Elegiste el tour con temática: Naturaleza Hostil")
+    } else if (eleccionTour === tourC) {
+        console.log("Elegiste el tour con temática: Zona de desastre")
+    } else if (eleccionTour === tourD) {
+        console.log("Elegiste el tour con temática: Experiencia paranormal")
+    } else {
+        console.error("No pudimos reconocer el código ingresado. Reinténtalo.")
+    }return
+}
+
+eleccionTour = seleccionarTour()
 
 function contabilizarViajeros() {
-    let cantidadViajeros = parseInt(prompt("Ingresa la cantidad de personas que viajarán"))
+    let cantidadViajeros = parseInt(prompt("Ingresa la cantidad de personas que desean realizar el tour"))
 
-    if (cantidadViajeros >= 1 && cantidadViajeros <= 9) {
+    if (cantidadViajeros != 0 && cantidadViajeros <= 9) {
         console.log("Se registra solicitud de hospedaje para: " + cantidadViajeros + " " + "personas")
-    } else if (cantidadViajeros >= 10 || cantidadViajeros <= 100) {
+    } else if (cantidadViajeros != 0 && cantidadViajeros >= 10) {
         console.log("Se registra solicitud de hospedaje para: " + cantidadViajeros + " " + "personas")
         alert("Contamos con un descuento de 20% para grupos a partir de 10 personas")
     } else {
-        console.error("La cantidad ingresada no responde a los parámetros establecidos.")
+        console.error("La cantidad ingresada no puede ser menor a 1 persona. Reintentalo.")
+        contabilizarViajeros()
     } return cantidadViajeros
 }
+cantidadViajeros = contabilizarViajeros()
 
 function definirTraslados() {
-    let gastoCompuesto = parseInt(prompt("Ingresa el monto máximo para gastos de pasajes, traslados y entradas generales"))
+    let gastoCompuesto = parseInt(prompt("Ingresa el monto máximo para gastos de: \n -pasajes, \n -traslados \n -entradas generales"))
 
     switch (gastoCompuesto) {
         case 35000:
@@ -24,16 +60,18 @@ function definirTraslados() {
             console.log("Has optado por pasajes de tarifa Standard")
             break
         case 120000:
-            console.log("Has optado por pasaje de tarifa Premium")
+            console.log("Has optado por pasajes de tarifa Premium")
             break
         default:
-            console.warn = "El valor ingresado no corresponde a ningún máximo establecido."
+            console.warn("El valor ingresado no corresponde a ningún máximo establecido. Ten presente no incluir caracteres especiales")
+            definirTraslados()
             break
     }return gastoCompuesto
 }
+gastoCompuesto = definirTraslados()
 
 function definirHospedaje() {
-    let gastoHospedaje = parseInt(prompt("Ingresa el monto máximo en concepto de hospedaje"))
+    let gastoHospedaje = parseInt(prompt("Ingresa el monto máximo para gastos de: \n -hospedaje"))
 
     switch (gastoHospedaje) {
         case 80000:
@@ -46,23 +84,41 @@ function definirHospedaje() {
             console.log("Has optado por modalidad de hospedaje Premium")
             break
         default:
-            console.error = "No pudimos registrar el valor ingresado, revisa los montos nuevamente."
+            console.error("El valor ingresado no corresponde a ningún máximo establecido. Ten presente no incluir caracteres especiales")
+            definirHospedaje()
             break
     }return gastoHospedaje
 }
+gastoHospedaje = definirHospedaje()
 
 /*Calculo sin descuento*/
 
-function calcularTotal() {
-    let costoParcial = parseInt((gastoCompuesto + gastoHospedaje) * cantidadViajeros)
-    console.log("El costo final de tu paquete de viaje para" + " " + cantidadViajeros + "personas es de $" + costoParcial + " " + "pesos")
-    confirm("¿Deseas continuar con el pago?")
+function continuarProceso() {
+    let continuarPago = confirm("¿Deseas continuar con el proceso?")
+
+    while (continuarPago === true) {
+        calcularCostoParcial()
+        continuarPago === false
+    }
 }
 
-alert("Armemos tu plan de gastos.")
+function calcularCostoParcial() {
+    let costoParcial = parseInt((gastoCompuesto + gastoHospedaje) * cantidadViajeros)
 
-cantidadViajeros = contabilizarViajeros()
-gastoCompuesto = definirTraslados()
-gastoHospedaje = definirHospedaje()
-costoParcial = calcularTotal()
+    console.log("El costo parcial de tu paquete de viaje para" + " " + cantidadViajeros + " " + "personas es de $" + " " + "pesos")
+}
+
+
+function agradecerCompra() {
+    console.log("Tu pago quedó registrado " + "¡disfruta tu viaje!")
+}
+
+
+continuarProceso()
+agradecerCompra()
+
+
+
+
+
 
